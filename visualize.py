7c5358c1c07a91538816f74b403054a7c75b8211
sayybro <shkim129@korea.ac.kr>
2023-01-21 13:48:58 +0000

    change visualize.py

--- visualize.py
+++ visualize.py
@@ -1,3 +1,23 @@
+import argparse
 import torch
+import cv2
+from main import get_args_parser
+from models import build_model
+from datasets import build_dataset
+
+
+class vis():
+
+    def __init__(self):
+    def 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser('annotation visualize', parents=[get_args_parser()])
+    args = parser.parse_args()
+    dataset_train = build_dataset(image_set='train', args=args)
+    
+    
+    build_dataset
+    model, _, postprocessors = build_model(args)
+    demo = Demo(args)
+    demo.save_video(args)
